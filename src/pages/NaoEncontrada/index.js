f8435060fc286a288ccbfbdbd3218dd9510a282a
LucianoVTN <luciano.vtn120@gmail.com>
2024-07-09 11:17:52 -0300

    inicio do projeto reactAluraFlix

---
+++ src/pages/NaoEncontrada/index.js
@@ -0,0 +1,11 @@
+import styles from './NaoEncontrada.module.css';
+
+function NaoEncontrada() {
+    return (
+        <section className={styles.container}>
+            <p>Item da pasquisa não foi encontrado</p>
+        </section>
+    )
+}
+
+export default NaoEncontrada;
